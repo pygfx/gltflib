c9c985b67b6d4637dbb77c5d92101ba0cadbdf37
Almar Klein <almar@almarklein.org>
2025-08-29 09:13:48 +0200

    Fix flit

--- gltflib/__init__.py
+++ gltflib/__init__.py
@@ -1,7 +1,17 @@
+version_info = (1, 0, 13)
+
+__version__ = ".".join(map(str, version_info))
+
 from .enums import *
 from .models import *
 from .utils import *
 from .gltf import GLTF
 from .gltf_resource import (
-    GLTFResource, FileResource, ExternalResource, GLBResource, Base64Resource, GLB_BINARY_CHUNK_TYPE,
-    GLB_JSON_CHUNK_TYPE)
+    GLTFResource,
+    FileResource,
+    ExternalResource,
+    GLBResource,
+    Base64Resource,
+    GLB_BINARY_CHUNK_TYPE,
+    GLB_JSON_CHUNK_TYPE,
+)
